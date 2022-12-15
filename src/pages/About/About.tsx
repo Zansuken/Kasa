85543230f6c8549fb4146fa4f7f34be1b43126c1
Zansuken <doom.sebastien.pro@gmail.com>
2022-12-15 22:59:15 +0100

    feat(About): added About

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,23 @@
-const About = () => <main>About</main>;
+import { FC } from 'react';
+import aboutData from '../../data/about.json';
+import coverImg from '../../assets/images/about-cover.png';
+
+import classes from './About.module.scss';
+import CollapsingCard from '../../components/shared/CollapsingCard/CollapsingCard';
+
+const About: FC = () => {
+  return (
+    <main className={classes['root']}>
+      <div className={classes['cover']}>
+        <img className={classes['coverImage']} src={coverImg} />
+      </div>
+      <section className={classes['aboutCards']}>
+        {aboutData.map(({ label, content }, index) => (
+          <CollapsingCard key={index} label={label} content={content} />
+        ))}
+      </section>
+    </main>
+  );
+};
 
 export default About;
