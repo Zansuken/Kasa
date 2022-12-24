80c530ce6bf850d60aa143dfb81cbfb9f9ed6f7a
Zansuken <doom.sebastien.pro@gmail.com>
2022-12-24 12:40:13 +0100

    Test(About): Added unit tests

---
+++ src/pages/About/About.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { CustomRouter } from '../../App';
+import About from './About';
+import aboutData from '../../data/about.json';
+
+describe('Render About correctly.', () => {
+  const renderAbout = () =>
+    render(
+      <CustomRouter>
+        <About />
+      </CustomRouter>
+    );
+
+  it('Render the cover', () => {
+    renderAbout();
+
+    const cover = screen.getByRole('img');
+
+    expect(cover).toBeInTheDocument();
+    expect(cover).toHaveAttribute('alt', 'Paysage montagneux');
+  });
+
+  it('Render the cards list.', () => {
+    renderAbout();
+
+    const list = screen.getAllByTestId('deploying-card-content');
+
+    expect(aboutData.length).toBe(list.length);
+
+    aboutData.forEach(({ label }) => {
+      expect(screen.getByText(label)).toBeInTheDocument();
+    });
+
+    screen.debug();
+  });
+});
